d395229b3156ec0668bb39bf18f1694ecaabf686
b08x <rwpannick@gmail.com>
2023-03-13 05:35:07 -0400

    set reaper_backup to only execute if ~/Library exists

--- roles/system/files/usr/local/bin/backup_reaper.sh
+++ roles/system/files/usr/local/bin/backup_reaper.sh
@@ -3,15 +3,20 @@
 declare -rx archive=REAPER-$(date +%Y%m%d%H).tar.gz
 declare -rx destination="$HOME/Library/reaper/"
 
-if [[ -f $destination/$archive ]]; then
-  echo "already backed up for the hour, hit o to overwrite"
-else
-  cd ~/.config/
-  tar -czvf $archive REAPER/
+if [[ -d $detination ]]; then
+	if [[ -f $destination/$archive ]]; then
+  	echo "already backed up for the hour, hit o to overwrite"
+	else
+  	cd ~/.config/
+  	tar -czvf $archive REAPER/
 
-  if [ $? = 0 ]; then
-    mv $archive ~/Library/reaper/
-  else
-    echo "archive did not complete"
-  fi
+  	if [ $? = 0 ]; then
+    	mv $archive ~/Library/reaper/
+  	else
+    	echo "archive did not complete"
+  	fi
+	fi
+else
+	echo "library not on this host, skipping task"
 fi
+		
